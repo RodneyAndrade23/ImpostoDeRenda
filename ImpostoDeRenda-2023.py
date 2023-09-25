6cd2f95666e432f7098e5a025d00d43e0fe7ee96
Rodney <rodneyandrade1@hotmail.com>
2023-09-24 23:57:32 -0300

    Initial commit

---
+++ ImpostoDeRenda-2023.py
@@ -0,0 +1,35 @@
+hora = int(input('Digite o valor da sua hora de trabalho: '))
+
+salario = hora * 8 * 5 * 4
+salario_bruto = salario
+
+if salario <= 2112:
+    print('Você está insento do Imposto de Renda!')
+    aux = 0
+
+elif salario > 2113 and salario <= 2826:
+    salario = salario - salario * 0.08
+    aux = 8
+else:
+    salario = salario - salario * 0.15
+    aux = 15
+
+salario = salario - salario * 0.09
+#ICSM
+
+salario = salario - salario * 0.08
+#IPVA
+
+print('=-=' * 15)
+print('IMPOSTO DO TRABALHADOR')
+print('-' * 15)
+print('Valor do salário bruto: R${:.2f}'.format(salario_bruto))
+print('-' * 15)
+print('IMPOSTOS:')
+print('Desconto do Imposto de Renda:',aux, '%')
+print('Desconto do ICSM: 9%')
+print('Desconto do IPVA: 7,5%')
+print('-' * 15)
+print('Valor do salário líquido: R${:.2f}'.format(salario))
+print('=-=' * 15)
+
